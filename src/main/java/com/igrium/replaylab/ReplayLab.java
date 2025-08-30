09732eebccc1996034dbaa5122afa819d92fd937
Igrium <thesam54123@gmail.com>
2025-08-29 22:35:54 -0700

    boilerplate

---
+++ src/main/java/com/igrium/replaylab/ReplayLab.java
@@ -0,0 +1,12 @@
+package com.igrium.replaylab;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReplayLab implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
