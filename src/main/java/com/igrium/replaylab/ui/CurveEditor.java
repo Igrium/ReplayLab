43d2a188c42c23bb551c250cdb302031f74e0ae6
Igrium <thesam54123@gmail.com>
2025-10-11 11:19:11 -0700

    added horizontal lines to curve editor

--- src/main/java/com/igrium/replaylab/ui/CurveEditor.java
+++ src/main/java/com/igrium/replaylab/ui/CurveEditor.java
@@ -6,16 +6,13 @@ import com.igrium.replaylab.scene.ReplayScene.KeyHandleReference;
 import com.igrium.replaylab.scene.obj.ReplayObject;
 import com.igrium.replaylab.ui.util.TimelineFlags;
 import com.igrium.replaylab.ui.util.TimelineHeader;
-import imgui.ImColor;
 import imgui.ImDrawList;
 import imgui.ImGui;
-import imgui.extension.implot.flag.ImPlotCol;
 import imgui.flag.ImGuiCol;
 import imgui.type.ImBoolean;
 import imgui.type.ImInt;
 import lombok.Getter;
 import lombok.Setter;
-import net.minecraft.util.math.ColorHelper;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
@@ -93,14 +90,12 @@ public class CurveEditor {
                                 Set<KeyHandleReference> selectedKeys, @Nullable ImInt playhead, int flags) {
         updatedObjects.clear();
 
-        float em = ImGui.getFontSize();
 
         int majorIntervalX = (int) TimelineHeader.computeMajorInterval(zoomFactorX);
         int minorIntervalX = majorIntervalX / 2;
-        int tinyIntervalX = majorIntervalX / 4;
 
-        boolean useTinyIntervalX = tinyIntervalX * zoomFactorY > em * 1.2;
-
+        float majorIntervalY = TimelineHeader.computeMajorInterval(zoomFactorY);
+        float minorIntervalY = majorIntervalY / 2;
 
         float headerCursorY = ImGui.getCursorPosY();
         float headerHeight = ImGui.getTextLineHeight() * 2f;
@@ -148,51 +143,60 @@ public class CurveEditor {
 
         // === GRAPH ===
 
-        ImGui.beginChild("keygraph", ImGui.getContentRegionAvailX(), graphHeight, false);
+        if (ImGui.beginChild("keygraph", ImGui.getContentRegionAvailX(), graphHeight, false)) {
+            // === SCROLL ===
+            if (ImGui.isWindowHovered()) {
+                float mWheel = ImGui.getIO().getMouseWheel();
+                if (mWheel != 0) {
+                    float factor = (float) Math.pow(2, mWheel * .125);
 
-        // === SCROLL ===
-        if (ImGui.isWindowHovered()) {
-            float mWheel = ImGui.getIO().getMouseWheel();
-            if (mWheel != 0) {
-                float factor = (float) Math.pow(2, mWheel * .125);
-                zoomFactorX = zoomFactorX * factor;
+                    if (ImGui.getIO().getKeyCtrl()) {
+                        zoomFactorY *= factor;
+                    } else {
+                        zoomFactorX *= factor;
+                    }
+                }
+            }
+
+            float graphX = ImGui.getCursorScreenPosX();
+            float graphY = ImGui.getCursorScreenPosY();
+
+            float gWidth = ImGui.getContentRegionAvailX();
+            float gHeight = ImGui.getContentRegionAvailY();
+
+            ImDrawList drawList = ImGui.getWindowDrawList();
+
+            // === BACKGROUND ===
+            drawList.addRectFilled(graphX, graphY, graphX + gWidth, graphY + gHeight, ImGui.getColorU32(ImGuiCol.FrameBg));
+
+            // Amount of milliseconds the graph is wide
+            float widthMs = gWidth / zoomFactorX;
+            int startTick = Math.floorDiv((int) offsetX, majorIntervalX) * majorIntervalX;
+            int endTick = Math.ceilDiv((int) (offsetX + widthMs), majorIntervalX) * majorIntervalX;
+
+            int colMajor = replaceAlpha(ImGui.getColorU32(ImGuiCol.Text), 48);
+            int colMinor = replaceAlpha(colMajor, 16);
+
+            // X intervals
+            for (int ms = startTick; ms <= endTick; ms+= minorIntervalX) {
+                float xPos = msToPixelX(ms) + graphX;
+                int color = ms % majorIntervalX == 0 ? colMajor : colMinor;
+                drawList.addLine(xPos, graphY, xPos, graphY + gHeight, color);
+            }
+
+            // Amount of units the graph is tall
+            float heightUnits = gHeight / zoomFactorY;
+
+            double startValue = Math.floor(offsetY / majorIntervalY) * majorIntervalY;
+            double endValue = Math.ceil((offsetY + heightUnits) / majorIntervalY) * majorIntervalY;
+
+            // Y intervals
+            for (double value = startValue; value <= endValue; value += minorIntervalY) {
+                float yPos = valueToPixelY(value) + graphY;
+                int color = value % majorIntervalY == 0 ? colMajor : colMinor;
+                drawList.addLine(graphX, yPos, graphX + gWidth, yPos, color);
             }
         }
-
-        float graphX = ImGui.getCursorScreenPosX();
-        float graphY = ImGui.getCursorScreenPosY();
-
-        float gWidth = ImGui.getContentRegionAvailX();
-        float gHeight = ImGui.getContentRegionAvailY();
-
-        ImDrawList drawList = ImGui.getWindowDrawList();
-
-        // === BACKGROUND ===
-        drawList.addRectFilled(graphX, graphY, graphX + gWidth, graphY + gHeight, ImGui.getColorU32(ImGuiCol.FrameBg));
-
-        // Amount of millseconds the graph is wide
-        float widthMs = gWidth / zoomFactorX;
-        int startTick = Math.floorDiv((int) offsetX, majorIntervalX) * majorIntervalX;
-        int endTick = Math.ceilDiv((int) (offsetX + widthMs), majorIntervalX) * majorIntervalX;
-
-        int colMajor = replaceAlpha(ImGui.getColorU32(ImGuiCol.Text), 48);
-        int colMinor = replaceAlpha(colMajor, 16);
-
-        // Major intervals
-        for (int ms = startTick; ms <= endTick; ms += majorIntervalX) {
-            float xPos = msToPixelX(ms) + graphX;
-            drawList.addLine(xPos, graphY, xPos, graphY + gHeight, colMajor);
-        }
-
-        // Minor intervals
-        for (int ms = startTick; ms <= endTick; ms += minorIntervalX) {
-            if (ms % majorIntervalX == 0)
-                continue;
-
-            float xPos = msToPixelX(ms) + graphX;
-            drawList.addLine(xPos, graphY, xPos, graphY + gHeight, colMinor);
-        }
-
         ImGui.endChild();
 
         // === HEADER ===
@@ -210,6 +214,14 @@ public class CurveEditor {
 
     private float pixelXToMs(float pixel) {
         return (float) (pixel / zoomFactorX + offsetX);
+    }
+
+    private float valueToPixelY(double value) {
+        return (float) ((value - offsetY) * zoomFactorY);
+    }
+
+    private double pixelYToValue(double pixel) {
+        return pixel / zoomFactorY + offsetY;
     }
 
     private static boolean hasFlag(int flag, int flags) {
