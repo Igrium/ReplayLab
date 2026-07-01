2447cc1d1155042a5d924abd4af7b49b024d2819
Igrium <thesam54123@gmail.com>
2026-07-01 12:53:07 -0700

    added confirmation when overwriting an exported file

--- src/main/java/com/igrium/replaylab/ui/ExportWindow.java
+++ src/main/java/com/igrium/replaylab/ui/ExportWindow.java
@@ -2,6 +2,7 @@ package com.igrium.replaylab.ui;
 
 import com.igrium.craftui.app.AppManager;
 import com.igrium.replaylab.editor.EditorState;
+import com.igrium.replaylab.render.RenderSettingsObj;
 import com.igrium.replaylab.scene.ReplayScene;
 import imgui.ImGui;
 import imgui.flag.ImGuiCond;
@@ -9,6 +10,8 @@ import imgui.flag.ImGuiWindowFlags;
 import imgui.type.ImBoolean;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Language;
+
+import java.nio.file.Files;
 
 public class ExportWindow {
 
@@ -26,17 +29,48 @@ public class ExportWindow {
             wantsOpen = false;
         }
 
+        boolean wantsClose = false;
+
         ImGui.setNextWindowSize(640, 0, ImGuiCond.Appearing);
         if (ImGui.beginPopupModal("Export Video", isOpen, ImGuiWindowFlags.NoSavedSettings)) {
-            editor.getScene().getRenderSettings().drawPropertiesPanel(editor);
+            RenderSettingsObj renderSettings = editor.getScene().getRenderSettings();
+            renderSettings.drawPropertiesPanel(editor);
+
             ImGui.separator();
+            String confirmKey = t("gui.replaylab.fileExists.header");
             if (ImGui.button(t("gui.ok"))) {
-                ImGui.closeCurrentPopup();
-                export(editor);
+
+                if (Files.isRegularFile(renderSettings.getOutPath())) {
+                    ImGui.openPopup(confirmKey);
+                } else {
+                    wantsClose = true;
+                    export(editor);
+                }
+            }
+
+            if (ImGui.beginPopupModal(confirmKey, ImGuiWindowFlags.AlwaysAutoResize | ImGuiWindowFlags.NoSavedSettings)) {
+                ImGui.text(tt("gui.replayLab.fileExists").formatted(renderSettings.getOutPath().getFileName().toString()));
+
+                if (ImGui.button(t("gui.ok"))) {
+                    ImGui.closeCurrentPopup();
+                    wantsClose = true;
+                    export(editor);
+                }
+                ImGui.setItemDefaultFocus();
+                ImGui.sameLine();
+                if (ImGui.button(t("gui.cancel"))) {
+                    ImGui.closeCurrentPopup();
+                }
+
+                ImGui.endPopup();
             }
 
             ImGui.sameLine();
             if (ImGui.button(t("gui.cancel"))) {
+                wantsClose = true;
+            }
+
+            if (wantsClose) {
                 ImGui.closeCurrentPopup();
             }
 
@@ -45,6 +79,7 @@ public class ExportWindow {
         }
     }
 
+
     private static void export(EditorState editor) {
         editor.getScene().saveObject(ReplayScene.RENDER_SETTINGS);
         editor.saveSceneAsync(); // Save our render settings
@@ -52,6 +87,10 @@ public class ExportWindow {
     }
 
     private static String t(String key) {
+        return Language.getInstance().get(key) + "###" + key;
+    }
+
+    private static String tt(String key) {
         return Language.getInstance().get(key);
     }
 }
